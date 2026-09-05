fb134dd7fb1482d863c9eed7785b3fc8d329aca0
SyncWithRaj <rajr127655@gmail.com>
2026-09-05 14:58:29 +0530

    chore: wire master data modules into AppModule and cleanup collections

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,22 @@ import { PrismaModule } from './prisma/prisma.module';
 import { MailModule } from './mail/mail.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
+import { WarehousesModule } from './warehouses/warehouses.module';
+import { DiscountRulesModule } from './discount-rules/discount-rules.module';
 
 @Module({
-  imports: [PrismaModule, MailModule, AuthModule, UsersModule],
+  imports: [
+    PrismaModule,
+    MailModule,
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    CustomersModule,
+    WarehousesModule,
+    DiscountRulesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
